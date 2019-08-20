f1ce63a3f034a6c0b474e33c08a072cac80fe509
Ookami <45501567+OokamiNoKami@users.noreply.github.com>
2019-08-20 16:21:49 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("Surveiller.");
+    console.log("Le bot a bien ete connecte");
+});
+
+bot.login(process.env.TOKEN);
